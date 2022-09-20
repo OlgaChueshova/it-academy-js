8098d9748e349b0715fb3582fec8adc691f8771c
olga <MY_NAME@example.com>
2022-09-20 21:35:41 +0300

    lcvbjgfikhjg

--- src/index.js
+++ src/index.js
@@ -4,3 +4,62 @@ const add123 = (a, b) => a + b;
 
 const res = add123(222, 3);
 console.log("Helloasda");
+
+
+const noReadMassage const unreadMessages
+const registration const isRegistered
+const favoriteGenres
+
+let i=0;
+
+while (i<10) {
+    i++;
+    console.log(i);
+}
+
+
+let i=0;
+
+do {
+    i++;
+    console.log(i);
+} while (i<10);
+
+
+let i="#";
+
+do {
+
+    if(i%2=0) {
+      i=" " ;
+      console.log(i); 
+    }
+    else {
+        i="#";
+        console.log(i);
+    }
+  
+} while (i.length<7);
+
+
+
+
+
+for (let i="#"; i.length<7; i+="#") {
+    console.log(i);
+}
+
+let i="#";
+
+while(i.length<7) {
+    i=i+="#";
+    console.log(i);
+};
+
+
+
+for (let i ="#"; i<8; )
+
+
+
+
