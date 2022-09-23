afef8971dc1359046b0521790902fc9988c89cfe
olga <MY_NAME@example.com>
2022-09-23 21:39:52 +0300

    lesson3

--- src/index.js
+++ src/index.js
@@ -1,65 +1,145 @@
-import "./main.scss";
+// import "./main.scss";
 
-const add123 = (a, b) => a + b;
+// const add123 = (a, b) => a + b;
 
-const res = add123(222, 3);
-console.log("Helloasda");
-
-
-const noReadMassage const unreadMessages
-const registration const isRegistered
-const favoriteGenres
-
-let i=0;
-
-while (i<10) {
-    i++;
-    console.log(i);
-}
+// const res = add123(222, 3);
+// console.log("Helloasda");
 
 
-let i=0;
+// // const unreadMessages
+// // const isRegistered
+// // const favoriteGenres
 
-do {
-    i++;
-    console.log(i);
-} while (i<10);
+// let i=0;
+
+// while (i<10) {
+//     i++;
+//     console.log(i);
+// }
 
 
-let i="#";
+// let i=0;
 
-do {
+// do {
+//     i++;
+//     console.log(i);
+// } while (i<10);
 
-    if(i%2=0) {
-      i=" " ;
-      console.log(i); 
+
+// let i="#";
+
+// do {
+
+//     if(i%2=0) {
+//       i=" " ;
+//       console.log(i); 
+//     }
+//     else {
+//         i="#";
+//         console.log(i);
+//     }
+  
+// } while (i.length<7);
+
+
+
+
+
+// for (let i="#"; i.length<7; i+="#") {
+//     console.log(i);
+// }
+
+// let i="#";
+
+// while(i.length<7) {
+//     i=i+="#";
+//     console.log(i);
+// };
+
+
+
+// for (let i ="#"; i<8; )
+
+
+// const currency = {
+//     usd: "dollar",
+//     blr: "bel rouble",
+//     rus: " russian rouble",
+// };
+
+// // const isCurrency = prompt('Введите валюту!');
+
+// // console.log(currency[isCurrency]);
+
+// let descriptor = Object.defineProperty(currency, "usd", {
+//     writable: false,
+//     enumerable: true,
+//     configurable: false,
+// })
+
+// descriptor.usd = "euro";
+
+// console.log(descriptor.usd);
+
+// for (let kay in currency) {
+//     console.log(currency[kay]);
+// }
+
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+
+// let sum = 0;
+
+// for (let kay in salaries) {
+//     sum += salaries[kay]
+// }
+// console.log(sum);
+
+
+// let a = 2;
+
+// function sum (a) {
+//     console.log(a*2);
+// }
+
+
+// function sayHi(...arg) {
+//     for (let nam of arg) {
+//         console.log(nam)
+//     }
+// }
+
+// sayHi("Olga", "Pete" )
+
+
+// let sum = (...rest) => {
+
+//     for (let i of rest) {
+//         return i+= i;
+//     }
+// }
+
+// let sum2 = sum(1, 2, 5, 10);
+
+// console.log(sum2);
+
+let a=1;
+
+let b=2;
+
+const min = (a, b) => {
+    if (a<b) {
+        return a
     }
     else {
-        i="#";
-        console.log(i);
+        return b
     }
-  
-} while (i.length<7);
-
-
-
-
-
-for (let i="#"; i.length<7; i+="#") {
-    console.log(i);
 }
 
-let i="#";
-
-while(i.length<7) {
-    i=i+="#";
-    console.log(i);
-};
-
-
-
-for (let i ="#"; i<8; )
-
-
+console.log(min);
 
 
